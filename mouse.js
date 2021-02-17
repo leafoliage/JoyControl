005b8baddec4dc6c4ed5f6347342c9442c8f13ed
Leafoliage <leafoliages@gmail.com>
2021-02-17 22:10:05 +0800

    [modify] mouse controller

--- mouse.js
+++ mouse.js
@@ -1,15 +1,19 @@
 const { Bot } = require('mousebot');
 
 let { mouse } = new Bot();
-const unitMove = 2;
+const unitMove = 0.1;
 let x = 500, y = 500;
 
 const mouseMove = (xAxis, yAxis) => {
-    const xMove = unitMove * xAxis
-    const yMove = unitMove * yAxis
-    mouse.moveSmooth(x, y, x + xMove, y + yMove);
+    if ((!xAxis || !yAxis) && xAxis!==0 && yAxis!==0) return
+    const xMove = Math.abs(xAxis) > 5 ? unitMove * xAxis : 0
+    const yMove = Math.abs(yAxis) > 5 ? unitMove * yAxis : 0
+    mouse.move(x+xMove, y+yMove)
     x += xMove;
     y += yMove;
 }
 
-module.exports = mouseMove
+module.exports = {
+    mouse,
+    mouseMove
+}
